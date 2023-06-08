f89a963317de1330f856b8fcb602cbf65de268a9
farhan-hoque <fhrubik98@gmail.com>
2023-06-08 15:09:58 +0600

    add multer

---
+++ src/Modules/result/result.models.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const resultSchema = new Schema({
+    point: Number,
+    refferd: [Number],
+})
+
+const rollSchema = new Schema({
+    roll: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    result: [resultSchema],
+})
+
+const ResultModel = mongoose.model('Result', rollSchema)
+
+module.exports = ResultModel
